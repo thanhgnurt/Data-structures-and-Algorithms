364b90fb8bfba42b64a72aa639201acab96c508e
Thanh Trung <thanhgnurt@gmail.com>
2020-12-27 19:50:42 +0700

    Array Manipulation

---
+++ Algorithm Pratice/Hacker_Rank/Array/Array Manipulation.cs
@@ -0,0 +1,72 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Array_Manipulation
+{
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries)
+    {
+        long[] result = new long[n];
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = 0;
+        }
+        long max = result[0];
+        for (int i = 0; i < queries.GetLength(0); i++)
+        {
+            for (int j = queries[i][0] - 1; j <= queries[i][1] - 1; j++)
+            {
+                result[j] += queries[i][2];
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (result[i] > max)
+            {
+                max = result[i];
+            }
+
+        }
+        return max;
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nm = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nm[0]);
+
+        int m = Convert.ToInt32(nm[1]);
+
+        int[][] queries = new int[m][];
+
+        for (int i = 0; i < m; i++)
+        {
+            queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
